3d6c2bcdd646f29cdfb9e41bc15a334bfe0d30f3
Mike Taylor <mike.charles.taylor@gmail.com>
2023-05-21 03:22:05 -0600

    Update scheduler

--- node-controller/pkg/scheduler.go
+++ node-controller/pkg/scheduler.go
@@ -2,13 +2,16 @@ package pkg
 
 import (
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Schedule struct {
-	executeDone bool
-	executeFunc func()
-	executeNext *Schedule
-	executeTime time.Time
+	executeDone       bool
+	executeFunc       func()
+	executeNext       *Schedule
+	executeTime       time.Time
+	executeRepeatedly time.Duration
 }
 
 type Scheduler struct {
@@ -44,31 +47,73 @@ func (scheduler *Scheduler) add(schedule *Schedule) {
 	}
 }
 
+func (scheduler *Scheduler) executeNextSchedule() {
+	if scheduler.executeNext.executeTime.Before(time.Now()) {
+		schedule := scheduler.executeNext
+		scheduler.executeNext = schedule.executeNext
+		schedule.executeFunc()
+		if schedule.executeRepeatedly != 0 {
+			schedule.executeTime.Add(schedule.executeRepeatedly)
+			scheduler.add(schedule)
+		} else {
+			schedule.executeDone = true
+		}
+	}
+}
+
 func (scheduler *Scheduler) run(schedules <-chan *Schedule) {
+	log.Info("Scheduler started")
 	for {
 		scheduler.schedule(schedules)
 	}
 }
 
 func (scheduler *Scheduler) schedule(schedules <-chan *Schedule) {
-	schedule := <-schedules
-	scheduler.add(schedule)
+	start := time.Now()
+	sleep := 1 * time.Hour
+
+	if scheduler.executeNext != nil {
+		if start.Before(scheduler.executeNext.executeTime) {
+			sleep = scheduler.executeNext.executeTime.Sub(start)
+		} else {
+			sleep = 0
+		}
+	}
+
+	select {
+	case schedule := <-schedules:
+		scheduler.add(schedule)
+		log.Info("Schedule added")
+	case <-time.After(sleep):
+	}
+
+	if scheduler.executeNext != nil {
+		scheduler.executeNextSchedule()
+	}
 
 	finish := time.Now()
-	if scheduler.executeNext != nil &&
-		scheduler.executeNext.executeTime.Before(finish) {
-		schedule := scheduler.executeNext
-		scheduler.executeNext = schedule.executeNext
-		schedule.executeFunc()
-		schedule.executeDone = true
-	}
+	elapsed := finish.Sub(start)
+	log.WithFields(log.Fields{
+		"elapsed": elapsed,
+	}).Info("Scheduling next")
 }
 
 func (s *Scheduler) ScheduleOnce(f func(), t time.Time) {
 	s.schedules <- &Schedule{
-		executeDone: false,
-		executeFunc: f,
-		executeNext: nil,
-		executeTime: t,
+		executeDone:       false,
+		executeFunc:       f,
+		executeNext:       nil,
+		executeTime:       t,
+		executeRepeatedly: 0,
+	}
+}
+
+func (s *Scheduler) ScheduleRepeat(f func(), t time.Time, r time.Duration) {
+	s.schedules <- &Schedule{
+		executeDone:       false,
+		executeFunc:       f,
+		executeNext:       nil,
+		executeTime:       t,
+		executeRepeatedly: r,
 	}
 }
--- node-controller/pkg/scheduler_test.go
+++ node-controller/pkg/scheduler_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,13 +13,42 @@ func TestAddSchedules(t *testing.T) {
 	s2 := &Schedule{executeTime: time.Now().Add(2 * time.Second)}
 	s3 := &Schedule{executeTime: time.Now().Add(3 * time.Second)}
 	s4 := &Schedule{executeTime: time.Now().Add(4 * time.Second)}
-	s := New()
-	s.add(s4)
-	s.add(s2)
-	s.add(s3)
-	s.add(s1)
-	assert.True(t, s.executeNext == s1)
-	assert.True(t, s.executeNext.executeNext == s2)
-	assert.True(t, s.executeNext.executeNext.executeNext == s3)
-	assert.True(t, s.executeNext.executeNext.executeNext.executeNext == s4)
+	scheduler := &Scheduler{}
+	scheduler.add(s4)
+	scheduler.add(s2)
+	scheduler.add(s3)
+	scheduler.add(s1)
+	assert.True(t, scheduler.executeNext == s1)
+	assert.True(t, scheduler.executeNext.executeNext == s2)
+	assert.True(t, scheduler.executeNext.executeNext.executeNext == s3)
+	assert.True(t, scheduler.executeNext.executeNext.executeNext.executeNext == s4)
+}
+
+func TestScheduler(t *testing.T) {
+	// Create a new scheduler
+	scheduler := New()
+
+	// Make channels to receive results
+	c1 := make(chan struct{})
+	c2 := make(chan struct{})
+	c3 := make(chan struct{})
+
+	// Schedule tasks to submit results
+	scheduler.ScheduleOnce(func() {
+		log.Info("Execute 3")
+		close(c3)
+	}, time.Now().Add(3*time.Millisecond))
+	scheduler.ScheduleOnce(func() {
+		log.Info("Execute 2")
+		close(c2)
+	}, time.Now().Add(2*time.Millisecond))
+	scheduler.ScheduleOnce(func() {
+		log.Info("Execute 1")
+		close(c1)
+	}, time.Now().Add(1*time.Millisecond))
+
+	// Wait for results from channels
+	<-c1
+	<-c2
+	<-c3
 }
