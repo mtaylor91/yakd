81be1cb65ed224e75d726d831177a93275f26815
Mike Taylor <contact@mtaylor.io>
2023-05-21 17:24:13 -0600

    WIP: debian post-bootstrapping

--- node-controller/pkg/debian/repositories.go
+++ node-controller/pkg/debian/repositories.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path"
 	"text/template"
+
+	"github.com/mtaylor91/yakd/node-controller/pkg/util"
 )
 
 const AptSourcesTemplate = `
@@ -12,24 +14,112 @@ deb http://{{.Mirror}} {{.Release}}-updates main contrib non-free
 deb http://security.debian.org/debian-security {{.Release}}-security main contrib non-free
 `
 
+const SignedAptSourcesTemplate = `
+deb [signed-by={{.Keyring}}] {{.Url}} {{.Components}}
+`
+
+const crioVersion = "1.24"
+const debianVersion = "Debian_11"
+const libcontainers = "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable"
+
+type AptSource struct {
+	Keyring    string
+	Url        string
+	Components string
+}
+
 // ConfigureRepositories configures the apt repositories for the target OS
 func (c *BootstrapConfig) ConfigureRepositories() error {
-	// Format the template
-	tmpl, err := template.New("sources.list").Parse(AptSourcesTemplate)
-	if err != nil {
-		return nil
+	keyrings := "usr/share/keyrings"
+
+	// Setup sources.list
+	if err := writeTemplateToFile("sources.list", AptSourcesTemplate,
+		path.Join(c.Target, "etc", "apt", "sources.list"), c); err != nil {
+		return err
 	}
 
-	// Create the sources.list file
-	f, err := os.Create(path.Join(c.Target, "etc", "apt", "sources.list"))
-	if err != nil {
-		return nil
+	// Setup kubernetes repository
+	keyring := path.Join(keyrings, "kubernetes-archive-keyring.gpg")
+	keyringDownload := util.NewDownload("https://packages.cloud.google.com/apt/doc/apt-key.gpg",
+		path.Join(c.Target, keyring))
+	// Download keyring
+	if err := keyringDownload.Download(); err != nil {
+		return err
+	}
+	// Write template to apt source file
+	if err := writeTemplateToFile("kubernetes.list", SignedAptSourcesTemplate,
+		path.Join(c.Target, "etc", "apt", "sources.list.d", "kubernetes.list"),
+		AptSource{
+			path.Join("/", keyring),
+			"https://apt.kubernetes.io/",
+			"kubernetes-xenial main",
+		},
+	); err != nil {
+		return err
 	}
 
-	// Execute the template
-	if err := tmpl.Execute(f, c); err != nil {
-		return nil
+	// Setup libcontainers repository
+	keyring = path.Join(keyrings, "libcontainers-archive-keyring.gpg")
+	releaseKeyringUrl := libcontainersUrl(debianVersion) + "Release.key"
+	keyringDownload = util.NewDownload(releaseKeyringUrl, path.Join(c.Target, keyring))
+	// Download keyring
+	if err := keyringDownload.DownloadAndDearmorGPG(); err != nil {
+		return err
+	}
+	// Write template to apt source file
+	if err := writeTemplateToFile("libcontainers.list", SignedAptSourcesTemplate,
+		path.Join(c.Target, "etc", "apt", "sources.list.d", "libcontainers.list"),
+		AptSource{
+			path.Join("/", keyring),
+			libcontainersUrl(debianVersion),
+			"/",
+		},
+	); err != nil {
+		return err
+	}
+
+	// Setup libcontainers crio repository
+	keyring = path.Join(keyrings, "libcontainers-crio-archive-keyring.gpg")
+	releaseKeyringUrl = crioArchiveUrl(crioVersion, debianVersion) + "Release.key"
+	keyringDownload = util.NewDownload(releaseKeyringUrl, path.Join(c.Target, keyring))
+	// Download keyring
+	if err := keyringDownload.DownloadAndDearmorGPG(); err != nil {
+		return err
+	}
+	// Write template to apt source file
+	if err := writeTemplateToFile("libcontainers-crio.list", SignedAptSourcesTemplate,
+		path.Join(c.Target, "etc", "apt", "sources.list.d", "libcontainers-crio.list"),
+		AptSource{
+			path.Join("/", keyring),
+			crioArchiveUrl(crioVersion, debianVersion),
+			"/",
+		},
+	); err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func writeTemplateToFile(name, src, dest string, data interface{}) error {
+	tmpl, err := template.New(name).Parse(src)
+
+	f, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	if err := tmpl.Execute(f, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func libcontainersUrl(os string) string {
+	return libcontainers + "/" + os + "/"
+}
+
+func crioArchiveUrl(version, os string) string {
+	return libcontainers + ":/cri-o:/" + version + "/" + os + "/"
 }
---
+++ node-controller/pkg/util/download.go
@@ -0,0 +1,69 @@
+package util
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+type Download struct {
+	Source      string
+	Destination string
+}
+
+// NewDownload initializes a new Download struct
+func NewDownload(source, destination string) *Download {
+	return &Download{source, destination}
+}
+
+// Download downloads a file from a URL to a destination
+func (d *Download) Download() error {
+	// Initiate http connection
+	resp, err := http.Get(d.Source)
+	if err != nil {
+		return err
+	}
+
+	// Create the destination file
+	f, err := os.Create(d.Destination)
+	if err != nil {
+		return err
+	}
+
+	// Copy the response body to the destination file
+	if _, err := io.Copy(f, resp.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DownloadAndDearmorGPG downloads a file from a URL to a destination
+// and performs a GPG dearmor
+func (d *Download) DownloadAndDearmorGPG() error {
+	// Initiate http connection
+	resp, err := http.Get(d.Source)
+	if err != nil {
+		return err
+	}
+
+	// Locate gpg command
+	gpg, err := exec.LookPath("gpg")
+	if err != nil {
+		return err
+	}
+
+	// Create the gpg dearmor command
+	cmd := exec.Command(gpg, "--dearmor", "-o", d.Destination)
+	cmd.Stdin = resp.Body
+	cmd.Stdout = os.Stderr
+	cmd.Stderr = os.Stderr
+
+	// Execute the command
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
