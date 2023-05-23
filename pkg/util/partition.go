b1bcef3244b713000744fad4bed93e75ec7781e8
Mike Taylor <contact@mtaylor.io>
2023-05-23 16:55:23 -0600

    Update parted commands to use script mode

--- pkg/util/partition.go
+++ pkg/util/partition.go
@@ -5,47 +5,47 @@ import log "github.com/sirupsen/logrus"
 func PartitionDisk(name string) error {
 	// Create partition table
 	log.Infof("Creating partition table on %s", name)
-	if err := RunCmd("parted", name, "mklabel", "gpt"); err != nil {
+	if err := RunCmd("parted", "-s", name, "mklabel", "gpt"); err != nil {
 		return err
 	}
 
 	// Create EFI partition
 	log.Infof("Creating EFI partition on %s", name)
-	if err := RunCmd("parted", name,
+	if err := RunCmd("parted", "-s", name,
 		"mkpart", "primary", "fat32", "1MiB", "512MiB"); err != nil {
 		return err
 	}
 
 	// Create root partition
 	log.Infof("Creating root partition on %s", name)
-	if err := RunCmd("parted", name,
+	if err := RunCmd("parted", "-s", name,
 		"mkpart", "primary", "ext4", "512MiB", "100%"); err != nil {
 		return err
 	}
 
 	// Set boot flag on EFI partition
 	log.Infof("Setting boot flag on EFI partition on %s", name)
-	if err := RunCmd("parted", name,
+	if err := RunCmd("parted", "-s", name,
 		"set", "1", "boot", "on"); err != nil {
 		return err
 	}
 
 	// Set esp flag on EFI partition
 	log.Infof("Setting esp flag on EFI partition on %s", name)
-	if err := RunCmd("parted", name,
+	if err := RunCmd("parted", "-s", name,
 		"set", "1", "esp", "on"); err != nil {
 		return err
 	}
 
 	// Set root partition label
 	log.Infof("Setting root partition label on %s", name)
-	if err := RunCmd("parted", name, "name", "2", "root"); err != nil {
+	if err := RunCmd("parted", "-s", name, "name", "2", "root"); err != nil {
 		return err
 	}
 
 	// Set EFI partition label
 	log.Infof("Setting EFI partition label on %s", name)
-	if err := RunCmd("parted", name, "name", "1", "efi"); err != nil {
+	if err := RunCmd("parted", "-s", name, "name", "1", "efi"); err != nil {
 		return err
 	}
 
