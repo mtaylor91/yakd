1cac60840f5938d655fcfc1b4f9d532196364de8
Mike Taylor <mike.charles.taylor@gmail.com>
2023-05-24 21:55:18 -0600

    Add --debug to all commands (enable debug logging)

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,28 @@
 package cmd
 
 import (
-	"github.com/mtaylor91/yakd/cmd/build"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-)
 
-var Root = &cobra.Command{
-	Use: "yakd",
-}
+	"github.com/mtaylor91/yakd/cmd/build"
+)
 
 func init() {
 	Root.AddCommand(build.Root)
+	f := Root.PersistentFlags()
+	f.Bool("debug", false, "Enable debug logging")
+}
+
+var Root = &cobra.Command{
+	Use:              "yakd",
+	PersistentPreRun: ConfigureRoot,
+}
+
+func ConfigureRoot(cmd *cobra.Command, args []string) {
+	f := cmd.Flags()
+	debug, _ := f.GetBool("debug")
+	if debug {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("Debug logging enabled")
+	}
 }
