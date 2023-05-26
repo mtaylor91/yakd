0cde32425ad34947ad6dfeed8469b1edf599ea73
Mike Taylor <mike.charles.taylor@gmail.com>
2023-05-26 07:45:09 -0600

    Add logging

--- pkg/debian/user.go
+++ pkg/debian/user.go
@@ -3,11 +3,15 @@ package debian
 import (
 	"context"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/mtaylor91/yakd/pkg/util/executor"
 )
 
 // configureAdminUser creates the admin user.
 func configureAdminUser(ctx context.Context, exec executor.Executor) error {
+	log.Infof("Configuring admin user")
+
 	if err := exec.RunCmd(ctx, "useradd", "-m", "-G", "sudo", "admin"); err != nil {
 		return err
 	}
